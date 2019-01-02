33b2268a2c289e2a7505237049c738e6f0b44ac0
saipujari <spujari@zillion.com>
2019-01-01 20:34:56 -0600

    Added basic programs

---
+++ DataTypesAndOperators/src/BasicPart_1/SumOfTwoNumbers.java
@@ -0,0 +1,15 @@
+package BasicPart_1;
+
+/**
+ * Created by Ramkrishna on 1/1/2019.
+ */
+public class SumOfTwoNumbers {
+    public static void main(String args[]) {
+
+        int A = 21;
+        int B = 31;
+int D = A+B;
+Hello.main();
+        System.out.println(D);
+    }
+}
